df3b7eeb3f56c8031936f8bc8bc591dd624b7331
Moore <146704702+Moore-Tian@users.noreply.github.com>
2024-04-01 04:26:50 +0800

    修改了作图的bug

--- graph.py
+++ graph.py
@@ -1,12 +1,7 @@
 import matplotlib.pyplot as plt
 
-x = [1, 2, 3, 4, 5]
-y = [1, 2, 3, 4, 5]
 
-plt.plot(x, y)
-plt.show()
-
-""" epochs = []
+epochs = []
 loss_values = []
 
 with open('train.log', 'r') as file:
@@ -17,11 +12,9 @@ with open('train.log', 'r') as file:
         epochs.append(epoch)
         loss_values.append(loss)
 
-epochs = [i for i in range(1, len(loss_values) + 1)]
-loss_values = [i for i in epochs]
-
 plt.plot(epochs, loss_values)
 plt.xlabel('Epoch')
 plt.ylabel('Loss')
 plt.title('Loss vs. Epoch')
-plt.savefig('loss_vs_epoch.png') """
+plt.savefig('loss_vs_epoch.png')
+plt.show()
